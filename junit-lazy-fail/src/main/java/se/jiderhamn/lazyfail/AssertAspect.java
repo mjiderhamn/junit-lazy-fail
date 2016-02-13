1af836b50c23b5327a3bb46270d174667d0c6d66
Mattias Jiderhamn <mattias.jiderhamn@readsoft.com>
2016-02-13 06:04:52 +0100

    Refactor into separate example module

--- junit-lazy-fail/src/main/java/se/jiderhamn/lazyfail/AssertAspect.java
+++ junit-lazy-fail/src/main/java/se/jiderhamn/lazyfail/AssertAspect.java
@@ -1,7 +1,6 @@
 package se.jiderhamn.lazyfail;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
